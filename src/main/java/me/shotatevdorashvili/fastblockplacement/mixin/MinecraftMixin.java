61cb038287e8386592859c952345253d0984a543
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-29 01:21:02 +0400

    Initial and Final commit

---
+++ src/main/java/me/shotatevdorashvili/fastblockplacement/mixin/MinecraftMixin.java
@@ -0,0 +1,18 @@
+package me.shotatevdorashvili.fastblockplacement.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(net.minecraft.client.Minecraft.class)
+public class MinecraftMixin {
+    @Shadow private int rightClickDelay;
+
+    @Inject( method = "startUseItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/InteractionHand;values()[Lnet/minecraft/world/InteractionHand;"))
+    private void onRightClick(CallbackInfo info)
+    {
+        this.rightClickDelay = 0;
+    }
+}
